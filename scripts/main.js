3818278f0daa6b1b0b0a03ac0e61ff9a347bdd95
Jesse Dillman <jesse@jdillman.com>
2016-12-24 14:17:45 -0600

    mobile menu

--- scripts/main.js
+++ scripts/main.js
@@ -21,4 +21,25 @@ jQuery(document).ready(function() {
 			}
 		});
 	};
+
+	// mobile nav
+	var mobileOpenState = false;
+
+	jQuery(window).resize(function() {
+		if (jQuery(window).width() > 760 ) {
+			sidebar.css({"transform":"translate(0px, 0px)"});
+			mobileOpenState = false;
+		}
+	});
+
+	jQuery('#mobileNav').click(function() {
+		if (mobileOpenState) {
+			sidebar.css({"transform":"translate(0px, 0px)"});
+			mobileOpenState = false;
+		} 
+		else{
+			sidebar.css({"transform":"translate(" + sidebar.outerWidth() + "px, 0px)"});
+			mobileOpenState = true;
+		};
+	});
 });
