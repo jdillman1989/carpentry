2457ae562ed39efe4a0a1d9bdd498270041e7212
Jesse Dillman <jesse@jdillman.com>
2016-12-23 18:12:24 -0600

    test feed

--- wp-content/themes/jadle/services/home-service.php
+++ wp-content/themes/jadle/services/home-service.php
@@ -16,7 +16,7 @@ class Home_Service {
 		$context = Timber::get_context();
 		$context['images_uri'] = _images_uri;
 		// $context['data'] = $this->data;
-		$context['home_content'] = get_the_content();
+		$context['home_content'] = '<p>This is a test for the home page</p>'
 		var_dump($context['home_content']);
 		Timber::render( theme_views . '/home.twig', $context);
 	}
