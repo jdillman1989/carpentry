62bad12066e75e1566b7945540c3416e49fbcef6
Jesse Dillman <jesse@jdillman.com>
2016-12-24 11:55:51 -0600

    error page

--- wp-content/themes/jadle/services/error-service.php
+++ wp-content/themes/jadle/services/error-service.php
@@ -14,8 +14,8 @@ class Error_Service {
 	public function load_view() {
 
 		$context = Timber::get_context();
-		Timber::render( theme_views . '/error.twig', $context);
 		$context['error_title'] = get_field('error_title', 'options');
 		$context['error_message'] = get_field('error_message', 'options');
+		Timber::render( theme_views . '/error.twig', $context);
 	}
 }
