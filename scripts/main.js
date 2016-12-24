2a00216ea9d9f958d723ad8c02bb8a182df55a0f
Jesse Dillman <jesse@jdillman.com>
2016-12-24 13:38:30 -0600

    responsive JS

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,24 @@
 jQuery(document).ready(function() {
 
+	// sticky sidebar
+	var sidebar = jQuery('.nav-section');
+	var content = jQuery('#global-content');
+	var sidebarHeight = sidebar.outerHeight();
+	var prevHeight = jQuery('.logo').outerHeight();
+	var sidebarScroll = "stick";
+
+	if (jQuery(window).width() > 760 && sidebar.outerHeight() < content.height() + 50) {
+
+		jQuery(window).scroll(function() {
+
+			if( jQuery(this).scrollTop() > prevHeight ) {
+
+				sidebar.addClass( sidebarScroll );
+			} 
+			else {
+
+				sidebar.removeClass( sidebarScroll );
+			}
+		});
+	};
 });
