edc3ab7a672e26595ee799fa0f4205a390342e3a
Jesse Dillman <jesse@jdillman.com>
2016-12-28 09:45:36 -0600

    Instagram feed integration

--- wp-content/themes/jadle/services/home-service.php
+++ wp-content/themes/jadle/services/home-service.php
@@ -21,7 +21,8 @@ class Home_Service {
 		// $context['data'] = $this->data;
 		$context['home_title'] = get_the_title();
 		$context['home_content'] = get_the_content();
-		$context['shortcode_content'] = do_shortcode($context['home_content']);
+		$context['include_feed'] = get_field('include_feed');
+		$context['feed_content'] = do_shortcode('[instagram-feed]');
 		Timber::render( theme_views . '/home.twig', $context);
 	}
 }
