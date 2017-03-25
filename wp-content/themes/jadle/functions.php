e1d502084d3d517b7a362d37928b2e1906c3b0d2
Jesse Dillman <jesse@jdillman.com>
2017-03-25 14:14:27 -0400

    fix img tags

--- wp-content/themes/jadle/functions.php
+++ wp-content/themes/jadle/functions.php
@@ -50,5 +50,10 @@ function jdillman_remove_submenu() {
 
 add_action( 'admin_menu', 'jdillman_remove_submenu', 999 );
 
+function filter_ptags_on_images($content){
+    return preg_replace('/<p>\\s*?(<a .*?><img.*?><\\/a>|<img.*?>)?\\s*<\\/p>/s', '\1', $content);
+}
+add_filter('the_content', 'filter_ptags_on_images');
+
 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
