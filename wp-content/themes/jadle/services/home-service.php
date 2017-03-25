58173b1b25d804c138ac6c3b994a4061cd1b5dd8
Jesse Dillman <jesse@jdillman.com>
2017-03-25 13:17:13 -0400

    homepage fixes

--- wp-content/themes/jadle/services/home-service.php
+++ wp-content/themes/jadle/services/home-service.php
@@ -22,6 +22,7 @@ class Home_Service {
 		$context['home_title'] = get_the_title();
 		$context['home_content'] = get_the_content();
 		$context['include_feed'] = get_field('include_feed');
+		$context['home_images'] = get_field('home_images');
 		$context['feed_content'] = do_shortcode('[instagram-feed]');
 		Timber::render( theme_views . '/home.twig', $context);
 	}
